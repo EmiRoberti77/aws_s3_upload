2e3cba24599b35c51e22875ce7ab663d0f33b34c
EmiRoberti77 <emiliano.roberti@accenture.com>
2023-08-27 11:21:34 +0100

    added express and endpoint to start trasnfer

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,7 @@
+import express, { Request, Response } from 'express';
+import { Server, Message, ROOT_ENDPOINT } from './util/Util';
+import { Logger, Level } from './logger/Logger';
+
 import { TransferService } from './service/TransferService';
 
 // const transfer_wegmans = new TransferService({
@@ -6,6 +10,31 @@ import { TransferService } from './service/TransferService';
 //   Body: 'data3.csv',
 //   Folder: 'Wegmans',
 // });
+const app = express();
+app.use(express.json());
 
-const transfer_wegmans = new TransferService();
-transfer_wegmans.transfer();
+app.get(ROOT_ENDPOINT.TRANSFER, (req: Request, res: Response) => {
+  res.status(200).send(
+    JSON.stringify({
+      message: Message.methodNotImplemented('GET'),
+    })
+  );
+  res.end();
+});
+
+app.post(ROOT_ENDPOINT.TRANSFER, async (req: Request, res: Response) => {
+  const transfer_wegmans = new TransferService(req.body);
+  await transfer_wegmans.transfer();
+
+  res.status(200).send(
+    JSON.stringify({
+      message: Message.success,
+      body: req.body,
+    })
+  );
+  res.end();
+});
+
+app.listen(Server.port, () => {
+  Logger.log(Message.serverStarted(Server.port));
+});
